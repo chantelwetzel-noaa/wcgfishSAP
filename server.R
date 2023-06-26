043643c1ea631b54cd4cd41e9883a175fb708b20
Hannah Chiu <hchiu03@uw.edu>
2023-06-26 13:45:36 -0700

    Changed default DT display to show top 10 rows, added dropdown menu

--- server.R
+++ server.R
@@ -1,194 +1,133 @@
 library(shiny)
 library(DT)
-#Read in Static commerical revenue file, maybe not best practice
-commerRevData <- read.csv("tables/commercial_revenue.csv", header = TRUE)
+#Read in Static commercial revenue file, maybe not best practice
+com_rev_data <- read.csv("tables/commercial_revenue.csv", header = TRUE)
 
 # Loaded Tribal Revenue Data - Will look to change 
-tribalData <- read.csv("tables/tribal_revenue.csv", header = TRUE)
+tribal_data <- read.csv("tables/tribal_revenue.csv", header = TRUE)
 
 #  Loaded Recreational Revenue Data - Will look to change 
-recData <- read.csv("tables/recreational_importance.csv", header = TRUE)
+rec_data <- read.csv("tables/recreational_importance.csv", header = TRUE)
 
 # Define server logic to display user inputs
 shinyServer(function(input, output) {
   
   #Gets Data from Commercial Revenue csv file and shows it on a Table
   #Table can Filter based on Species name or Ascend order by Rank
-  output$commerdataViewer <- DT::renderDataTable({
+  output$com_data_viewer <- DT::renderDataTable({
     # Renamed CSV Columns
-    commerRevColumns <- c("Species", 
-                          "Rank", 
-                          "Factor Score", 
-                          "Interum Value",  
-                          "Revenue",
-                          "California Revenue",
-                          "Oregon Revenue",
-                          "Washington Revenue")
+    com_rev_col <- c("Species", 
+                      "Rank", 
+                      "Factor Score", 
+                      "Interum Value",  
+                      "Revenue",
+                      "California Revenue",
+                      "Oregon Revenue",
+                      "Washington Revenue")
     
     # Change data
-    commerRevData <- commerRevData[commerRevData$Species %in% input$commSpeciesSelector,]
+    com_rev_data <- com_rev_data[com_rev_data$Species %in% input$com_species_selector,]
     
-    
-    # Format Data: Rounding Decimal Places and specifying top "x" List
-    datatable(commerRevData, options(lengthMenu = 
-                                       list( c(-1, 5, 10, 20), #ALL is a Keyword
-                                             c("All", 5, 10, 20)
-                                            ),
-              order = list(list(1, 'asc'))),
-              colnames = commerRevColumns,
-              rownames = FALSE
-    )%>% formatRound(3:ncol(commerRevData), 2)%>% 
-      formatCurrency(5:ncol(commerRevData),
-                     currency = "$",
-                     digits =0
-      )%>%
-      formatStyle(
-        columns = "Species",
-        backgroundColor = "#5F9EA0",
-        color = "white",
-        fontWeight = "bold"
-      )%>%
-      formatStyle('Rank', 
-                  background = styleInterval(
-                    cuts = c(10, 20, 30 , 40, 50, 60),  
-                    values = c("#F4F9F4" , 
-                               "#A7D7C5",
-                               "#74B49B",
-                               "#5C8D89",
-                               "#698474",
-                               "#2C5D63",
-                               "#283739")
-                  ),
-                  color = styleInterval(
-                    cuts = c(30),
-                    values = c("black", "white")
-                  )
-      )
+    # create commercial rev. datatable output
+    datatable(com_rev_data, options = options(lengthMenu = list(c(10, 20, 30, -1),
+                                                                c(10, 20, 30, "All")),
+                                              order = list(list(1, "asc"))),
+              class = "cell-border stripe", rownames = FALSE, colnames = com_rev_col) %>%
+      
+      # round numeric entries + add $ sign for monetary values
+      formatRound(3:ncol(com_rev_data), 2) %>%
+      formatCurrency(5:ncol(com_rev_data), currency = "$", digits = 0) %>%
+      
+      # assign color coding
+      formatStyle(columns = "Species", backgroundColor = "#5F9EA0",
+                  color = "white", fontWeight = "bold") %>%
+      formatStyle("Rank", background = styleInterval(cuts = c(10, 20, 30, 40, 50, 60),
+                                                     values = c("#F4F9F4", "#A7D7C5",
+                                                                "#74B49B", "#5C8D89",
+                                                                "#698474", "#2C5D63",
+                                                                "#283739")),
+                  color = styleInterval(cuts = c(30), values = c("black", "white")))
   })
-  
-  # Commercial Species Info Window
-  output$commInfoWindow <- renderPrint({
-    input$comspecInfo
-  })
-  
   
   #Can display and filter Tribal Fish Data by name and rank value
-  output$tribaldataViewer <- DT::renderDataTable({
+  output$tribal_data_viewer <- DT::renderDataTable({
     # Renamed CSV Columns
-    tribalColumns <- c("Species", 
-                       "Rank", 
-                       "Factor Score", 
-                       "Subsistence Score",  
-                       "Initial Factor Score",
-                       "Interum Value",
-                       "Revenue"
-    )
-    
-    # Change data
-    tribalData <- tribalData[tribalData$Species %in% input$tribalSpeciesSelector,]
-    
-    
-    # Format Data: Rounding Decimal Places and specifying top "x" List
-    datatable(tribalData, options(lengthMenu = list(
-                                                    c(-1, 5, 10, 20), 
-                                                    c("All", 5, 10, 20)
-                                                    ),
-             order = list(list(1, 'asc'))),
-             colnames = tribalColumns,
-             rownames = FALSE
-    )%>% formatRound(5:ncol(tribalData), 2)%>%
-      formatRound(3, 2)%>%
-      formatCurrency(ncol(tribalData),
-                     currency = "$",
-                     digits = 0)%>%
-      formatStyle(
-        columns = "Species",
-        backgroundColor = "#6a5acd",
-        color = "white",
-        fontWeight = "bold"
-      )%>% 
-      formatStyle('Rank', 
-                           background = styleInterval(
-                             cuts = c(10, 20, 30 , 40, 50, 60),  
-                             values = c("#F4F9F4" , 
-                                        "#A7D7C5",
-                                        "#74B49B",
-                                        "#5C8D89",
-                                        "#698474",
-                                        "#2C5D63",
-                                        "#283739")
-                           ),
-                           color = styleInterval(
-                             cuts = c(30),
-                             values = c("black", "white")
-                           )
-      )
-  })
-  
-  # Tribal Species Info Window
-  output$tribalInfoWindow <- renderPrint({
-    tribalData[input$tribalspecInfo]
-  })
-  
-  #Can display and filter Recreational Fish Data by name and rank value
-  output$recdataViewer <- DT::renderDataTable({
-    # Renamed CSV Columns
-    recColumns <- c("Species", 
+    tribal_col <- c("Species", 
                     "Rank", 
                     "Factor Score", 
+                    "Subsistence Score",  
                     "Initial Factor Score",
-                    "Pseudo Value Coast Wide",
-                    "Pseudo Value California",
-                    "Pseudo Value Oregon",
-                    "Pseudo Value Washington",
-                    "Relative Weight California",
-                    "Relative Weight Oregon",
-                    "Relative Weight Washington",
-                    "Retained Catch Coast Wide",
-                    "Retained Catch California",
-                    "Retained Catch Oregon",
-                    "Retained Catch Washington"
-    )
+                    "Interum Value",
+                    "Revenue")
     
     # Change data
-    recData <- recData[recData$Species %in% input$recSpeciesSelector,]
+    tribal_data <- tribal_data[tribal_data$Species %in% input$tribal_species_selector,]
     
     
-    # Format Data: Rounding Decimal Places and specifying top "x" List
-    datatable(recData, options(lengthMenu =  list( c(-1, 5, 10, 20), #ALL is a Keyword
-                                                   c("All", 5, 10, 20)
-                                                  ),
-                               order = list(list(1, 'asc'))),
-              colnames = recColumns,
-              rownames = FALSE
-    )%>% formatRound(5:ncol(recData), 0)%>%
-      formatRound(3:4, 2)%>%
-      formatRound(2, 0)%>%
-      formatCurrency(ncol(recData),
-                     currency = "$",
-                     digits = 0)%>%
-      formatStyle(
-        columns = "Species",
-        backgroundColor = "#fd5c63",
-        color = "white",
-        fontWeight = "bold"
-      )%>%
-      formatStyle('Rank', 
-                  background = styleInterval(
-                                  cuts = c(10, 20, 30 , 40, 50, 60),  
-                                  values = c("#F4F9F4" , 
-                                             "#A7D7C5",
-                                             "#74B49B",
-                                             "#5C8D89",
-                                             "#698474",
-                                             "#2C5D63",
-                                             "#283739")
-                                ),
-                  color = styleInterval(
-                                  cuts = c(30),
-                                  values = c("black", "white")
-                          )
-                  )
+    # create tribal datatable output
+    datatable(tribal_data, options(lengthMenu = list(c(10, 20, 30, -1), 
+                                                     c(10, 20, 30, "All")),
+                                   order = list(list(1, "asc"))),
+              class = "cell-border stripe", rownames = FALSE, colnames = tribal_col) %>%
+      
+      # round numeric entries + add $ sign to monetary values
+      formatRound(5:ncol(tribal_data), 2) %>%
+      formatRound(3, 2) %>%
+      formatCurrency(ncol(tribal_data), currency = "$", digits = 0) %>%
+      
+      # assign color coding
+      formatStyle(columns = "Species", backgroundColor = "#6A5ACD", color = "white",
+                  fontWeight = "bold") %>% 
+      formatStyle("Rank", background = styleInterval(cuts = c(10, 20, 30 , 40, 50, 60),  
+                                                     values = c("#F4F9F4", "#A7D7C5",
+                                                                "#74B49B", "#5C8D89",
+                                                                "#698474", "#2C5D63",
+                                                                "#283739")),
+                  color = styleInterval(cuts = c(30), values = c("black", "white")))
   })
   
+  # Can display and filter Recreational Fish Data by name and rank value
+  output$rec_data_viewer <- DT::renderDataTable({
+    # Renamed CSV Columns
+    rec_col <- c("Species", 
+                 "Rank", 
+                 "Factor Score", 
+                 "Initial Factor Score",
+                 "Pseudo Value Coast Wide",
+                 "Pseudo Value California",
+                 "Pseudo Value Oregon",
+                 "Pseudo Value Washington",
+                 "Relative Weight California",
+                 "Relative Weight Oregon",
+                 "Relative Weight Washington",
+                 "Retained Catch Coast Wide",
+                 "Retained Catch California",
+                 "Retained Catch Oregon",
+                 "Retained Catch Washington")
+    
+    # Change data
+    rec_data <- rec_data[rec_data$Species %in% input$rec_species_selector,]
+    
+    # create recreational datatable output
+    datatable(rec_data, options(lengthMenu = list(c(10, 20, 30, -1), #ALL is a Keyword
+                                                  c(10, 20, 30, "All")),
+                                order = list(list(1, "asc"))),
+              class = "cell-border stripe", rownames = FALSE, colnames = rec_col) %>%
+      
+      # round numeric entries + add $ sign to monetary values
+      formatRound(5:ncol(rec_data), 0) %>%
+      formatRound(3:4, 2) %>%
+      formatRound(2, 0) %>%
+      formatCurrency(ncol(rec_data), currency = "$", digits = 0) %>%
+      
+      # assign color coding
+      formatStyle(columns = "Species", backgroundColor = "#FD5C63", color = "white",
+                  fontWeight = "bold") %>%
+      formatStyle("Rank", background = styleInterval(cuts = c(10, 20, 30, 40, 50, 60),
+                                                     values = c("#F4F9F4", "#A7D7C5",
+                                                                "#74B49B", "#5C8D89",
+                                                                "#698474", "#2C5D63",
+                                                                "#283739")),
+                  color = styleInterval(cuts = c(30), values = c("black", "white")))
+  })
 })
